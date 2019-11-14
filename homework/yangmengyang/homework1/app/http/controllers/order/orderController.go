46f50825354c4d4313bf0af895fca968cef822f1
yangmengyang <yangmengyang@qudian.com>
2019-11-14 22:21:25 +0800

    第二周作业

--- homework/yangmengyang/homework1/app/http/controllers/order/orderController.go
+++ homework/yangmengyang/homework1/app/http/controllers/order/orderController.go
@@ -4,15 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/qit-team/snow-core/log/logger"
 	"homework/go_weekly_practise/homework/yangmengyang/homework1/app/http/controllers"
-	"github.com/qit-team/snow-core/utils/httputil"
 	"fmt"
+	"github.com/qit-team/snow-core/utils/httputil"
 )
 
 func Servicea(c *gin.Context) {
 	logger.Debug(c, "hello", "test message")
 	fmt.Println("hello a")
-	dict := map[string]interface{}{"param_a": "aaaa", "param_b": "bbb"}
-	httputil.PostJson(c, "localhost:8080/service_b", dict)
+	postJsonHw(c)
+	fmt.Println("goodby a")
 	controllers.Success(c, "hello servicea!")
 	return
+}
+
+func postJsonHw(c *gin.Context) {
+	dict := map[string]interface{}{"name": "aaaa", "mobile": "12345678901"}
+	result, err := httputil.PostJson(c, "http://localhost:8080/service_b", dict)
+	if err != nil {
+		fmt.Println("exception", err)
+	}
+	fmt.Println("success", result)
 }
